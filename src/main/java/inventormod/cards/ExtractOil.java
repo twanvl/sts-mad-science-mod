c377815073848376d06cf1ee2e94348287dd8490
Twan van Laarhoven <twanvl@gmail.com>
2018-05-08 20:59:34 +0200

    ExtractOil rework: it is now an attack

--- src/main/java/inventormod/cards/ExtractOil.java
+++ src/main/java/inventormod/cards/ExtractOil.java
@@ -1,13 +1,15 @@
 package inventormod.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import inventormod.powers.ExtractOilPower;
 
@@ -17,20 +19,20 @@ public class ExtractOil extends AbstractInventorCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int WEAK_AMT = 1;
-    private static final int UPGRADE_WEAK_AMT = 1;
-    private static final CardType TYPE = CardType.SKILL;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_DAMAGE = 3;
+    private static final CardType TYPE = CardType.ATTACK;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
 
     public ExtractOil() {
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-        this.magicNumber = this.baseMagicNumber = WEAK_AMT;
+        this.baseDamage = DAMAGE;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new WeakPower(m, this.magicNumber, false), this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m,new DamageInfo(p, this.damage, this.damageTypeForTurn),AbstractGameAction.AttackEffect.SLASH_VERTICAL));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new ExtractOilPower(p,1), 1));
     }
 
@@ -43,7 +45,7 @@ public class ExtractOil extends AbstractInventorCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(UPGRADE_WEAK_AMT);
+            this.upgradeDamage(UPGRADE_DAMAGE);
         }
     }
 }
