ddd2bf1d1901cc7267c6133a4f79294e361007c5
Twan van Laarhoven <twanvl@gmail.com>
2018-05-08 21:41:16 +0200

    Cleanup

--- src/main/java/inventormod/powers/CaptureMomentumPower.java
+++ src/main/java/inventormod/powers/CaptureMomentumPower.java
@@ -1,7 +1,6 @@
 package inventormod.powers;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
