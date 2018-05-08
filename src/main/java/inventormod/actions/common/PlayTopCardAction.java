deff8b25d309c9178544226102ba2c974adcfc01
Twan van Laarhoven <twanvl@gmail.com>
2018-05-08 21:15:51 +0200

    Implemented Postpone

--- src/main/java/inventormod/actions/common/PlayTopCardAction.java
+++ src/main/java/inventormod/actions/common/PlayTopCardAction.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.actions.utility.QueueCardAction;
 import com.megacrit.cardcrawl.actions.utility.UnlimboAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
--- src/main/java/inventormod/cards/Postpone.java
+++ src/main/java/inventormod/cards/Postpone.java
@@ -8,11 +8,15 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import inventormod.actions.common.HandToTopOfDrawPileAction;
+
 public class Postpone extends AbstractInventorCard {
     public static final String ID = "Postpone";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 0;
     private static final int DRAW = 1;
     private static final CardType TYPE = CardType.SKILL;
@@ -26,7 +30,11 @@ public class Postpone extends AbstractInventorCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        // TODO: PlaceCardsOnTopDrawPile
+        if (this.upgraded) {
+            AbstractDungeon.actionManager.addToBottom(new HandToTopOfDrawPileAction(0, 99, false));
+        } else {
+            AbstractDungeon.actionManager.addToBottom(new HandToTopOfDrawPileAction(1, 1, false));
+        }
     }
 
     @Override
@@ -43,6 +51,8 @@ public class Postpone extends AbstractInventorCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 }
