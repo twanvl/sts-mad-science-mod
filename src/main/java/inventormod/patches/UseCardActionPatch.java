3539386d5063821b9319bab6e61ed373c09ab226
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 13:18:26 +0200

    Put Postpone on top of draw pile

---
+++ src/main/java/inventormod/patches/UseCardActionPatch.java
@@ -0,0 +1,15 @@
+package inventormod.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+
+import inventormod.cards.Postpone;
+
+@SpirePatch(cls="com.megacrit.cardcrawl.actions.utility.UseCardAction", method="ctor")
+public class UseCardActionPatch {
+    public static void Postfix(Object _self, Object card, Object target) {
+        if (card instanceof Postpone) {
+            ((UseCardAction)_self).reboundCard = true;
+        }
+    }
+}
