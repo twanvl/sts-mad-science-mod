ee39b4f6e78d2df20f253d98726f962ca9280297
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 13:18:12 +0200

    Action for Poke

---
+++ src/main/java/inventormod/actions/unique/PokeAction.java
@@ -0,0 +1,62 @@
+package inventormod.actions.unique;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.SoulGroup;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class PokeAction extends AbstractGameAction {
+    private float startingDuration;
+    private ArrayList<AbstractCard> drawnCards;
+
+    public PokeAction() {
+        this(new ArrayList<>());
+    }
+    private PokeAction(ArrayList<AbstractCard> drawnCards) {
+        this.drawnCards = drawnCards;
+        this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
+        this.duration = this.startingDuration = Settings.FAST_MODE ? Settings.ACTION_DUR_XFAST : Settings.ACTION_DUR_FASTER;
+    }
+
+    @Override
+    public void update() {
+        // Draw a card, keep track of drawn cards
+        if (SoulGroup.isActive()) {
+            return;
+        }
+        int deckSize = AbstractDungeon.player.drawPile.size();
+        int discardSize = AbstractDungeon.player.discardPile.size();
+        if (deckSize == 0 && discardSize > 0 && !(!drawnCards.isEmpty() && drawnCards.get(drawnCards.size()-1).cost == 0)) {
+            // continue after shuffle
+            AbstractDungeon.actionManager.addToTop(new PokeAction(drawnCards));
+            AbstractDungeon.actionManager.addToTop(new EmptyDeckShuffleAction());
+            this.isDone = true;
+            return;
+        }
+        this.duration -= Gdx.graphics.getDeltaTime();
+        if (this.duration < 0.0f) {
+            if ((!drawnCards.isEmpty() && drawnCards.get(drawnCards.size()-1).cost == 0) || AbstractDungeon.player.drawPile.isEmpty()) {
+                // found a 0 cost card, or no more cards to draw
+                if (drawnCards.size() > 0) drawnCards.remove(drawnCards.size()-1);
+                for (AbstractCard c : drawnCards) {
+                    AbstractDungeon.player.hand.moveToDiscardPile(c);
+                    c.triggerOnManualDiscard();
+                    GameActionManager.incrementDiscard(false);
+                }
+                this.isDone = true;
+            } else {
+                drawnCards.add(AbstractDungeon.player.drawPile.getTopCard());
+                AbstractDungeon.player.draw();
+                AbstractDungeon.player.hand.refreshHandLayout();
+                // draw again, or check if done
+                this.duration = startingDuration;
+            }
+        }
+    }
+}
