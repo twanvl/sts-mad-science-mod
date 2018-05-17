b41cf4726f0c8bf1786dc4165d86e18219a3ee4a
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 13:16:07 +0200

    LabAccident rework

--- src/main/java/inventormod/cards/LabAccident.java
+++ src/main/java/inventormod/cards/LabAccident.java
@@ -16,17 +16,19 @@ public class LabAccident extends AbstractInventorCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 2;
+    private static final int AMOUNT = 5;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.RARE;
 
     public LabAccident() {
         super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, CardTarget.SELF);
         this.exhaust = true;
+        this.magicNumber = this.baseMagicNumber = AMOUNT;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new LabAccidentAction(this.upgraded));
+        AbstractDungeon.actionManager.addToBottom(new LabAccidentAction(this.upgraded, this.magicNumber));
     }
 
     @Override
