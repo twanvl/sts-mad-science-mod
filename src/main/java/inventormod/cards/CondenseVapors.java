03e9f27cc2dda7420469ab1f35a95233c0ccd0bf
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 21:03:52 +0200

    slight nerf to gold gained

--- src/main/java/inventormod/cards/CondenseVapors.java
+++ src/main/java/inventormod/cards/CondenseVapors.java
@@ -17,7 +17,7 @@ public class CondenseVapors extends AbstractInventorCard {
     private static final int COST = 1;
     private static final int UPGRADED_COST = 0;
     private static final int FUEL_COST = 1;
-    private static final int GOLD_AMT = 25;
+    private static final int GOLD_AMT = 20;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF;
