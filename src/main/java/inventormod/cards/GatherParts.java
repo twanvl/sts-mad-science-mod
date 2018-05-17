eac5b7d7b381be4429715ce2815583a8c0464ca3
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 23:17:38 +0200

    GatherParts+: no longer random

--- src/main/java/inventormod/cards/GatherParts.java
+++ src/main/java/inventormod/cards/GatherParts.java
@@ -31,7 +31,7 @@ public class GatherParts extends AbstractInventorCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new AllTrinketsToHandAction());
-        AbstractDungeon.actionManager.addToBottom(new ShuffleTrinketAction(this.magicNumber, true, true));
+        AbstractDungeon.actionManager.addToBottom(new ShuffleTrinketAction(this.magicNumber, upgraded ? false : true, true));
     }
 
     @Override
