5450f742855e812735e0898dd382019c01d0b43b
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 21:05:00 +0200

    cleanup

--- src/main/java/inventormod/cards/GoldenGun.java
+++ src/main/java/inventormod/cards/GoldenGun.java
@@ -2,11 +2,9 @@ package inventormod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
