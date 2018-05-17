fd663e7eb53909ff62e1d565292b9ca5892fe27b
Twan van Laarhoven <twanvl@gmail.com>
2018-05-17 21:04:42 +0200

    cleanup

--- src/main/java/inventormod/actions/common/DrawAndDiscardAllButOneAction.java
+++ src/main/java/inventormod/actions/common/DrawAndDiscardAllButOneAction.java
@@ -10,7 +10,6 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.CardGroup.CardGroupType;
 import com.megacrit.cardcrawl.cards.SoulGroup;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
