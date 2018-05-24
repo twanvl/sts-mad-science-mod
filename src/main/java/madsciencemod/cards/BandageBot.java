9e5938b454df5e765728c228b2749717ae8e4858
Twan van Laarhoven <twanvl@gmail.com>
2018-05-24 23:15:44 +0200

    Added tooltip to BandageBot

--- src/main/java/madsciencemod/cards/BandageBot.java
+++ src/main/java/madsciencemod/cards/BandageBot.java
@@ -1,5 +1,8 @@
 package madsciencemod.cards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -8,6 +11,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import basemod.helpers.TooltipInfo;
 import madsciencemod.powers.BandageBotPower;
 
 public class BandageBot extends AbstractMadScienceCard {
@@ -15,6 +19,7 @@ public class BandageBot extends AbstractMadScienceCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 2;
     private static final int UPGRADED_COST = 1;
     private static final CardType TYPE = CardType.POWER;
@@ -28,6 +33,13 @@ public class BandageBot extends AbstractMadScienceCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new BandageBotPower(p, 1), 1));
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltips() {
+        List<TooltipInfo> tips = new ArrayList<>();
+        tips.add(new TooltipInfo(EXTENDED_DESCRIPTION[0],EXTENDED_DESCRIPTION[1]));
+        return tips;
     }
 
     @Override
