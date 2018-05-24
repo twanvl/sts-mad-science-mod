c235504daec375a8a1006cdaf9dce87b04e886b4
Twan van Laarhoven <twanvl@gmail.com>
2018-05-24 22:20:32 +0200

    nerf cost

--- src/main/java/madsciencemod/cards/MechanicalSnecko.java
+++ src/main/java/madsciencemod/cards/MechanicalSnecko.java
@@ -15,8 +15,8 @@ public class MechanicalSnecko extends AbstractMadScienceCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 2;
-    private static final int UPGRADED_COST = 1;
+    private static final int COST = 3;
+    private static final int UPGRADED_COST = 2;
     private static final CardType TYPE = CardType.POWER;
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF;
