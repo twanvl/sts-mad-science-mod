1ce8de060c46c61985191a9f732e5f14d5cdef3f
Twan van Laarhoven <twanvl@gmail.com>
2018-06-27 01:11:35 +0200

    Reduced FlammableWall's fuel gain

--- src/main/java/madsciencemod/cards/FlammableWall.java
+++ src/main/java/madsciencemod/cards/FlammableWall.java
@@ -20,9 +20,9 @@ public class FlammableWall extends AbstractMadScienceCard {
     public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 2;
     private static final int FUEL_COST = 1;
-    private static final int FUEL_AMT = 3;
+    private static final int FUEL_AMT = 2;
     private static final int UPGRADE_FUEL_AMT = 1;
-    private static final int BLOCK_AMT = 15;
+    private static final int BLOCK_AMT = 16;
     private static final int UPGRADE_BLOCK_AMT = 3;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
