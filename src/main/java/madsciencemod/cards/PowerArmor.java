ee7b49dc9fd1283123306f2425c852fc2ac972eb
Twan van Laarhoven <twanvl@gmail.com>
2018-07-15 15:10:39 +0200

    Buffed Power Armor

--- src/main/java/madsciencemod/cards/PowerArmor.java
+++ src/main/java/madsciencemod/cards/PowerArmor.java
@@ -16,7 +16,7 @@ public class PowerArmor extends AbstractMadScienceCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int AMOUNT = 3;
+    private static final int AMOUNT = 4;
     private static final int UPGRADE_AMOUNT = 2;
     private static final CardType TYPE = CardType.POWER;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
